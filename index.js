e7da9fc020df9a63ec93c5232ff292a9181a2618
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-10-12 19:25:53 +0900

    Replace mdns with multicast-dns

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const Client = require('castv2-client').Client;
 const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
 const googleTTS = require('google-tts-api');  // TODO: replace this with the official one. https://github.com/googleapis/nodejs-text-to-speech
 const { program } = require('commander');
+const multicastdns = require('multicast-dns');
+
+function findGoogleHome() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            reject('timeout');
+        }, 5000);
+
+        const mdns = multicastdns();
+        mdns.on('response', function (response) {
+            console.debug('response =', response);
+            let ip = undefined;
+            let port = undefined;
+            for (const additional of response.additionals) {
+                if (additional.type == 'A') ip = additional.data;
+                if (additional.type == 'SRV') port = additional.data.port;
+            }
+            if (ip !== undefined && port != undefined) {
+                resolve([ip, port]);
+                mdns.destroy();
+            }
+        });
+        mdns.query({
+            questions: [{
+                name: '_googlecast._tcp.local',
+                type: 'PTR',
+            }]
+        });
+    });
+}
 
 const say = (host, media) => {
     const client = new Client();
@@ -32,17 +62,30 @@ async function main() {
     // parse args
     program
         .usage('--ip IP [--language LANGUAGE] TEXT...')
-        .requiredOption('--ip <IP>', 'IP address to your device')
+        .option('--ip <IP>', 'IP address to your device')
         .option('--port <PORT>', 'port for your device')
         .option('--language <LANGUAGE>', 'language for Text-to-Speach (default: en-US)', 'en-US')
         .option('--speed <SPEED>', 'speed for Text-to-Speach (default: 1.0)', parseFloat, 1.0);
     program.parse(process.argv);
-    if (!program.args) {
-        throw 'TEXT is not given';
+
+    const text = program.args.join(' ').trim();
+    if (!text) {
+        console.error('error: the argument `TEXT` is empty');
+        process.exit(1);
     }
-    const text = program.args.join(' ');
-    console.log('Options:', program.opts());
-    console.log('Text:', text);
+
+    let ip = program.ip;
+    let port = program.port;
+    if (ip === undefined) {
+        if (port !== undefined) {
+            console.error('error: the option `--port` is specified while the option `--ip` is not specified');
+            process.exit(1);
+        }
+        [ip, port] = await findGoogleHome().catch((err) => {
+            console.error('error: failed to find Google Home devices:', err);
+            process.exit(1);
+        });
+    }
 
     // build the query
     const url = await googleTTS(text, program.language, program.speed);
@@ -52,8 +95,8 @@ async function main() {
         streamType: 'BUFFERED',
     };
     const options = {
-        host: program.ip,
-        port: program.port,
+        host: ip,
+        port: port,
     };
     console.log(options);
     console.log(media);
